5351bb386abf9780fbf9f63f82afa85e6a8ecc75
ngohuy123 <huynqgch17251@fpt.edu.vn>
2020-04-13 03:28:08 +0700

    chat-font-end

--- Public/User/js/chat.js
+++ Public/User/js/chat.js
@@ -11,12 +11,12 @@ $(document).ready(() => {
     user_full
   })
   socket.on('chat_tutor', (data)=>{
-    let tutor = "<div Id ='"+data.tutor_id+"' class='a-user'><img src='/static/images/"+data.tutor_avatar+"' alt='sunil' class='chat_img'><span class='chat_ib'>"+data.tutor_full+"</span></div>"
+    let tutor = "<div Id ='"+data.tutor_id+"' class='a-user'><img src='/static/images/"+data.tutor_avatar+"' alt='sunil' class='chat_img'><div class='chat_title'>"+data.tutor_full+"</div><div class='chat_date'>Dec 25</div><br><div class='chat_ib'>Test, which is a new approach to have all solutions astrology under one roof.</div></div>"
       $('#chat_user').append(tutor)
   })
   socket.on('chat_student', (data)=>{
     data.students.forEach(student =>{
-      let user = "<div Id ='"+student._id+"' class='a-user'><img src='/static/images/"+student.User_avatar+"' alt='sunil' class='chat_img'><span class='chat_ib'>"+student.User_full+"</span></div>"
+      let user = "<div Id ='"+student._id+"' class='a-user'><img src='/static/images/"+student.User_avatar+"' alt='sunil' class='chat_img'><div class='chat_title'>"+student.User_full+"</div><div class='chat_date'>Dec 25</div><br><div class='chat_ib'>Test, which is a new approach to have all solutions astrology under one roof.</div></div>"
       $('#chat_user').append(user)
     })
   })
